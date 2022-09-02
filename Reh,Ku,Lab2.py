6ab27c64069f025f688960b40d1b92d53f822cd7
ku-reh <112183529+ku-reh@users.noreply.github.com>
2022-09-02 08:45:30 -0500

    Add files via upload

---
+++ Reh,Ku,Lab2.py
@@ -0,0 +1,19 @@
+
+
+Temp=eval(input("Enter temp:"))
+Conv=9/5*Temp+32
+print("The temp in Fehrenheit is:", Conv)
+
+
+firstnum=float(input("Enter number:"))
+secondnum=float(input("Enter number:"))
+average=(firstnum+secondnum)/2
+print("The average is:", average)
+
+print('3+4')
+print('The value of 3+4 is',3+4,'.')
+print('The value of 3+4 is',3+4,'.',sep='')
+print('On the first line')
+print('On the second line')
+print('On the first line',end='')
+print('On the second line')
